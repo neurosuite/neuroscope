4d181ab2d39b95d868f8ab8c2cf4e2de1b69355c
Florian Franzen <FlorianFranzen@gmail.com>
2015-10-16 21:57:49 +0200

    Build as .app bundle on OS X

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,6 +3,9 @@ CMAKE_CONFIG_VARS="-DWITH_CEREBUS=ON"
 if [ $QT_VERSION = "qt4" ]; then
     CMAKE_CONFIG_VARS="${CMAKE_CONFIG_VARS} -DWITH_QT4=ON"
 fi
+if [ $TRAVIS_OS_NAME = "osx" ]; then
+    CMAKE_CONFIG_VARS="${CMAKE_CONFIG_VARS} -DAPPBUNDLE=ON"
+fi
 
 mkdir build
 cd build
