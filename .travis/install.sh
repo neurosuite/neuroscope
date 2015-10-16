25b61502485460dc98e09249c87ebb2ff3c791dc
Florian Franzen <FlorianFranzen@gmail.com>
2015-10-16 20:44:08 +0200

    Fix travis build script for qt4 and make install script executable

--- .travis/install.sh
+++ .travis/install.sh
@@ -27,16 +27,21 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     fi
 fi
 
+CMAKE_CONFIG_VARS=""
+if [ $QT_VERSION = "qt4" ]; then
+    CMAKE_CONFIG_VARS="${CMAKE_CONFIG_VARS} -DWITH_QT4=ON"
+fi
+
 git clone https://github.com/neurosuite/libneurosuite.git
 cd libneurosuite
-cmake .
+cmake $CMAKE_CONFIG_VARS .
 make
 sudo make install
 cd ..
 
 git clone https://github.com/neurosuite/libcbsdk.git
 cd libcbsdk
-cmake .
+cmake $CMAKE_CONFIG_VARS .
 make
 sudo make install
 cd ..
