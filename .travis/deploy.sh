b8f059c51a567e3f36f17c6ddc0b915b1d725f62
Florian Franzen <FlorianFranzen@gmail.com>
2015-10-20 20:53:38 +0200

    Travis: Fix deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-export RELEASE_PKG_FILE=""
+export ENABLE_DEPLOYMENT=false
 if [ "$TRAVIS_OS_NAME" = "linux" ] &&
-   [ "$QT_VERSION"     = "qt4" ]; then
-   export RELEASE_PKG_FILE=$(ls "${TRAVIS_BUILD_DIR}/build/neuroscope_*.deb");
+   [ "$QT_VERSION" = "qt4" ]; then
+   export ENABLE_DEPLOYMENT=true
 fi
 if [ "$TRAVIS_OS_NAME" = "osx" ] &&
-   [ "$QT_VERSION"     = "qt5" ]; then
-   export RELEASE_PKG_FILE=$(ls "${TRAVIS_BUILD_DIR}/build/neuroscope-*.dmg");
+   [ "$QT_VERSION" = "qt5" ]; then
+   export ENABLE_DEPLOYMENT=true
 fi
