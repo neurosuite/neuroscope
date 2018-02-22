fb6b6cb95e0affe33c3573cf910fbdefb60ac973
Florian Franzen <Florian.Franzen@gmail.com>
2018-02-22 03:09:26 +0100

    Fix CI on OS X and Qt5 Linux

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-set -e
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    brew update && brew install qt
-    export PATH="/usr/local/opt/qt/bin:$PATH"
+    brew update && brew install qt@5.5 || exit 1
+    export PATH="/usr/local/opt/qt@5.5/bin:$PATH"
 fi
 
 CMAKE_CONFIG_VARS=""
@@ -11,12 +10,12 @@ if [ $QT_VERSION = "qt4" ]; then
     CMAKE_CONFIG_VARS="${CMAKE_CONFIG_VARS} -DWITH_QT4=ON"
 fi
 
-git clone https://github.com/neurosuite/libneurosuite.git
+git clone https://github.com/neurosuite/libneurosuite.git || exit 1
 cd libneurosuite
-cmake $CMAKE_CONFIG_VARS . && sudo make install
+cmake $CMAKE_CONFIG_VARS . && sudo make install || exit 1
 cd ..
 
-git clone https://github.com/neurosuite/libcbsdk.git
+git clone https://github.com/neurosuite/libcbsdk.git || exit 1
 cd libcbsdk
-cmake $CMAKE_CONFIG_VARS . && sudo make install
+cmake $CMAKE_CONFIG_VARS . && sudo make install || exit 1
 cd ..
